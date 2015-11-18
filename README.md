dff6f7f2b1e8bc367d373a16bf249d7ad958f283
dbbbit <dbbbit@gmail.com>
2015-11-18 22:44:43 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+The Dot Config files
+===
+
--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
-#!sh
+#!/bin/bash
 
 # Get Vundle for vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+rm -rf ~/.vimrc
+ln -s $(pwd)/vimrc ~/.vimrc
 
 # then do `:PluginInstall` in vim to get all plugins installed
